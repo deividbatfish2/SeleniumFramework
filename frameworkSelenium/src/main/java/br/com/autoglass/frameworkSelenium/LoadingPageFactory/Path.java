98c7bbe421bd854e8704479fac0da2d12dc81800
Deivid de Assis Teixeira <deivid.teixeira@autoglass.com.br>
2017-03-02 15:52:50 -0300

    LoadingPageFactory
    Pacote criado para permitir a utilização de LoadableComponent e
    PageFactory juntos.

---
+++ frameworkSelenium/src/main/java/br/com/autoglass/frameworkSelenium/LoadingPageFactory/Path.java
@@ -0,0 +1,12 @@
+package br.com.autoglass.frameworkSelenium.LoadingPageFactory;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Path {
+String value();
+}
---
+++ frameworkSelenium/src/main/java/br/com/autoglass/frameworkSelenium/LoadingPageFactory/Verify.java
@@ -0,0 +1,15 @@
+package br.com.autoglass.frameworkSelenium.LoadingPageFactory;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Verify {
+	String INVALID_TITLE = "\0";
+	String INVALID_XPATH = "\0";
+	String title() default INVALID_TITLE;
+	String xpath() default INVALID_XPATH;
+}
